1dc1bca7e3f78720b9ce1023fad2723aadad7021
Jasmin Rubinovitz <jasrub@gmail.com>
2017-01-19 15:18:36 -0500

    first commit

---
+++ magpie/config.py
@@ -0,0 +1,27 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CORPUS_DIR = os.path.join(ROOT_DIR, 'data', 'hep-keywords')
+
+# word2vec & scaler
+EMBEDDING_SIZE = 100
+WORD2VEC_MODELPATH = os.path.join(
+    CORPUS_DIR,
+    'w2v_models',
+    'word2vec{0}_model.gensim'.format(EMBEDDING_SIZE)
+)
+SCALER_PATH = os.path.join(
+    CORPUS_DIR,
+    'scalers',
+    'scaler_nn_{0}.pickle'.format(EMBEDDING_SIZE)
+)
+
+# Models
+NN_ARCHITECTURE = 'cnn'
+
+# Training parameters
+BATCH_SIZE = 64
+NB_EPOCHS = 1
+
+# Number of tokens to save from the abstract, zero padded
+SAMPLE_LENGTH = 200
---
+++ train.py
@@ -0,0 +1,30 @@
+from magpie import MagpieModel
+import magpie.utils
+import json
+import gensim
+
+# Load Google's pre-trained Word2Vec model.
+print "Loading pre-trained word to vec model"
+word2vecmodel = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)
+print "model_loaded"
+labels = []
+with open('labels.json') as data_file:
+  labels = json.load(data_file)
+print ("%d labels"%len(labels))
+print labels[:20]
+train_dir = "./train"
+model = MagpieModel(word2vec_model=word2vecmodel, labels=labels)
+print "initializing word vectors..."
+
+#magpie.utils.save_to_disk('embeddings', model.word2vec_model)
+magpie.utils.save_to_disk('./scalar/scalar', model.scaler, overwrite=True)
+
+print "embeddings saved to disk"
+print "training for 5 epochs"
+print (model)
+model.fit_scaler('./train')
+magpie.utils.save_to_disk('./scalar/scalar', model.scaler, overwrite=True)
+model.batch_train(train_dir, word2vecmodel.vocab.keys(), test_dir='./test', nb_epochs=5)
+magpie.utils.save_to_disk('./scalar/scalar', model.scaler, overwrite=True)
+
+model.keras_model.save('trained_model.h5')
